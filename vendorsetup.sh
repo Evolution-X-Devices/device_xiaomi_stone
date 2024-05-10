b6245f24ee47d879403a85717124058815c28a49
Arijit78 <sahaarijit2007@gmail.com>
2024-05-10 17:54:31 +0000

    stone: init vendorsetup.sh
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Clone Vendor
+git clone https://github.com/Arijit78/vendor_xiaomi_stone_new.git -b udc vendor/xiaomi/stone
+
+# Clone prebuilt kernel
+git clone https://github.com/Arijit78/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
+
+# Clone Dolby
+git clone https://github.com/Arijit78/vendor_dolby_oplus.git -b udc vendor/dolby
+
+# Clone hardware xiaomi
+git clone https://github.com/Evolution-X/hardware_xiaomi.git -b udc hardware/xiaomi
+
+# Clone Kernel Source
+git clone https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/xiaomi/sm6375
