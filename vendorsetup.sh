2421d6ce330883bdfdb27117ccbc966007ab3107
Arijit78 <sahaarijit2007@gmail.com>
2024-07-16 11:30:10 +0000

    stone: Official EvolutionX BringUp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,14 @@
-# Clone Vendor
-git clone https://github.com/Arijit78/vendor_xiaomi_stone_new.git -b udc vendor/xiaomi/stone
-
-# Clone prebuilt kernel
-git clone https://github.com/Arijit78/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
-
 # Clone Dolby
 git clone https://github.com/Arijit78/vendor_dolby_oplus.git -b udc vendor/dolby
 
 # Clone hardware xiaomi
-git clone https://github.com/Evolution-X/hardware_xiaomi.git -b udc hardware/xiaomi
+git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 14.0 hardware/xiaomi
 
 # Clone Kernel Source
 git clone https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/xiaomi/sm6375
+
+# Clone prebuilt kernel
+git clone https://github.com/Arijit78/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
 
 # Clone v4a
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
