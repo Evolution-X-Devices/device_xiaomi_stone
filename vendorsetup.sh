04976e30ab934b0d2b54e28c15e87754c0b8711f
Arijit78 <sahaarijit2007@gmail.com>
2024-07-16 17:55:38 +0000

    stone: Remove duplicate entries from genfs_contexts - Also update kernel link from crdroid in vendorsetup.sh
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Arijit78/vendor_dolby_oplus.git -b udc vendor/dolby
 git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 14.0 hardware/xiaomi
 
 # Clone Kernel Source
-git clone https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/xiaomi/sm6375
+git clone https://github.com/crdroidandroid/android_kernel_qcom_sm8350.git -b 14.0 kernel/xiaomi/sm6375
 
 # Clone prebuilt kernel
 git clone https://github.com/Arijit78/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
