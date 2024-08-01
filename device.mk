9c4da389cf8228d8d22699e18f22197044341066
bgcngm <bgcngm@users.noreply.github.com>
2024-08-01 06:48:54 +0200

    stone: move to new RFS install_symlink targets
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ endif
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Device uses high-density artwork where available
