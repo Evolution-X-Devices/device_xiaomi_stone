dc5503034b744638e2828c9b984ecbb3fddf2818
Arijit78 <sahaarijit2007@gmail.com>
2024-08-06 18:38:04 +0000

    stone: Don't track v4a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,3 @@ git clone https://github.com/crdroidandroid/android_kernel_qcom_sm8350.git -b 14
 
 # Clone prebuilt kernel
 git clone https://github.com/Arijit78/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
-
-# Clone v4a
-git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
